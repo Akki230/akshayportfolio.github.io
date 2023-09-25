63cc44af16393c290b9250c625049abf3ab950b0
Akshay Kumar <akshaysaini3080@gmail.com>
2023-09-25 16:44:30 +0530

    portfolio created

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HeaderComponent } from './layouts/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SlickCarouselModule
+  ],
+  exports:[
+    HeaderComponent,
+    FooterComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+   slideConfig = {
+    slidesToShow: 1, 
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: false,
+    fade: true,
+    cssEase: 'linear',
+    infinite: true
+  };
+ 
+  
+ 
+
+}
