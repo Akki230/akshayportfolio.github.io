6ec1943f45796dbb09bbc7d107ce89664b29075c
Akshay Kumar <akshaysaini3080@gmail.com>
2023-09-27 16:45:40 +0530

    changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
   slideConfig = {
     slidesToShow: 1,
     slidesToScroll: 1,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 2000,
     arrows: false,
     dots: false,
